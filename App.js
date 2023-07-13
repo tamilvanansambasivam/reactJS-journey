10fc7483d7e2a27e441d125dc7aa36ab0ece7d51
tamilvanan <tamilvanan@mac.local>
2023-07-13 15:47:08 +0530

    day 3

--- App.js
+++ App.js
@@ -1,30 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" },
-    [
-        React.createElement("div", { id: "child" },
-        [
-            React.createElement("h1", {}, "I'm calm"),
-            React.createElement("h1", {}, "I'm the best"),
-            React.createElement("h2", {}, "I'm lucky")
-        ]), 
-        
-        React.createElement("div", { id: "child2" },
-            [
-                React.createElement("h1", {}, "I'm happy"),
-                React.createElement("h2", {}, "Relax...")
-            ])]
+// React.createElement => JS Object => HTMLElement(render)
 
+
+//  
+
+//JSX - HTML or XML like syntax (JSX is not html inside the js)
+//JSX - (transpiled before it reaches the JS engine) - PARCEL - babel
+
+// JSX => React.createElement => JS Object => HTMLElement(render)
+
+
+const jsxHeading = (
+
+<h1 className="heading">
+    Hello react using JSX 🚀
+</h1>
+  
+
+    )
+
+//components 
+// there are two types of components 
+// i) functional and ii) class 
+
+
+const Title = () =>  {
+   return (
+    <h1>Hello React functional components (Title)</h1>
+    )
+}
+
+const Title2 = () =>  
+  (
+     <h1>Hello React functional components (Title2) </h1>
+)
+ 
+
+
+const HeadingComponents = ()=> (  
+
+    <div>
+    {title}
+    {Title()}
+    
+
+    <Title />
+    <Title2 />
+    <h1>Hello React functional components (HeadingComponents) </h1>
+
+    </div>)
+
+const title = (
+<>
+<h1>Its title element</h1>
+
+</>
 )
 
-console.log(parent)
-
+  
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-
-
-
+root.render(  <HeadingComponents />) 
